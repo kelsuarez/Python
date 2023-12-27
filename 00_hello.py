2410e75c07ab5c3698c23c6581db5eb3e5ede6fa
KELVIN <kelsuarez22@gmail.com>
2023-12-27 22:57:47 +0100

    varibles

---
+++ 00_hello.py
@@ -0,0 +1,32 @@
+# Comentario (Hola mundo)
+
+"""
+este 
+es
+otro
+comentario
+"""
+
+
+print("Hello mundo")
+
+print('Se pueden utilisar dobles o simples para string')
+
+
+
+# Consultar el tipo de dato
+print(type('hehe'))
+#Seria tipo 'str'
+
+print(type(5))
+#Seria tipo 'int'
+
+print(type(1.5))
+#Seria tipo float
+
+print(type(True))
+#Seria tipo 'bool'
+
+print(type(1 + 1j))
+#Seria tipo 'complex'
+
---
+++ 01_variables.py
@@ -0,0 +1,5 @@
+# VARIABLES
+# Todas las variables se escriben en minuscula 
+
+myvariable = "My string variable"
+print(myvariable)
