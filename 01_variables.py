d9e2d5179b1ffb71fd7c07b0ac27be681b2e79d7
KELVIN <kelsuarez22@gmail.com>
2024-01-03 23:51:36 +0100

    vav

--- 01_variables.py
+++ 01_variables.py
@@ -1,5 +1,21 @@
 # VARIABLES
 # Todas las variables se escriben en minuscula 
 
-myvariable = "My string variable"
-print(myvariable)
+my_variable = "My string variable"
+print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+# Concatenacion de variables en un print
+print(my_variable, my_int_variable, my_bool_variable)
+# Si le coloco una , puedo adjuntar varias variables en una sola 
+
+my_int_str_variable = str(my_int_variable)
+print(my_int_str_variable)
+print(type(my_int_str_variable))
+
+# Al utilizar un str convertimos la variable my_int_variable que era un numero 5 en string
