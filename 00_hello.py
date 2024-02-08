8d73cea6aa2045b58751444fd0ac226612b263cf
KELVIN <kelsuarez22@gmail.com>
2024-02-08 16:25:24 +0100

    Principios

--- 00_hello.py
+++ 00_hello.py
@@ -9,24 +9,15 @@ comentario
 
 
 print("Hello mundo")
-
 print('Se pueden utilisar dobles o simples para string')
 
 
 
 # Consultar el tipo de dato
-print(type('hehe'))
-#Seria tipo 'str'
 
-print(type(5))
-#Seria tipo 'int'
-
-print(type(1.5))
-#Seria tipo float
-
-print(type(True))
-#Seria tipo 'bool'
-
-print(type(1 + 1j))
-#Seria tipo 'complex'
-
+print(type('hehe')) #Seria tipo 'str'
+print(type(5)) #Seria tipo 'int'
+print(type(1.5)) #Seria tipo float
+print(type(True)) #Seria tipo 'bool'
+print(type(1 + 1j)) #Seria tipo 'complex'
+print(type(print("Mi cadena de texto"))) # Tipo 'NoneType
